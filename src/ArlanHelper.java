9de56b3ff9fd9c4520ab7d2199ae9272edf3fd2c
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-12-25 06:28:51 +0200

    tasks

--- src/ArlanHelper.java
+++ src/ArlanHelper.java
@@ -1,21 +1,39 @@
 public class ArlanHelper {
     //Test output
-
     public String drawQuad(int n) {
-        String string = "";
-        int counter = 0;
-        while (counter < n) {
-            string += "*";
-            counter++;
+        String result = "";
 
+        int lines = n;
+
+        while(lines > 0) {
+            lines--;
+
+            int columns = n;
+            while(columns > 0) {
+                result += "*";
+                columns--;
+            }
+
+            result += "\n";
         }
-       int counter1 = 0;
-        while (counter1 < n){
-            System.out.println(string);
-            counter1++;
-        }
-        return "";
+
+        return result;
     }
+//    public String drawQuad(int n) {
+//        String string = "";
+//        int counter = 0;
+//        while (counter < n) {
+//            string += "*";
+//            counter++;
+//
+//        }
+//       int counter1 = 0;
+//        while (counter1 < n){
+//            System.out.println(string);
+//            counter1++;
+//        }
+//        return "";
+//    }
 
     //    Напиши метод public String drawQuad(int n), который рисует символами '*' квадрат со стороной n.
     public static void main(String[] args) {
