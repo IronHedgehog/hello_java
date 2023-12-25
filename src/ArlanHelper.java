94bc16b89d91e9e19bbe8782ad10132cf9ab9a97
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-12-25 05:44:05 +0200

    tasks

---
+++ src/ArlanHelper.java
@@ -0,0 +1,16 @@
+public class ArlanHelper {
+    //Test output
+
+    public String drawQuad(int n) {
+        return "";
+    }
+
+    //    Напиши метод public String drawQuad(int n), который рисует символами '*' квадрат со стороной n.
+    public static void main(String[] args) {
+        ArlanHelper helper = new ArlanHelper();
+        //Should be:
+        //**
+        //**
+        System.out.println(helper.drawQuad(2));
+    }
+}
--- src/CheapStocks.java
+++ src/CheapStocks.java
@@ -1,3 +1,44 @@
+import java.util.Arrays;
+
 public class CheapStocks {
 
+
+    public String getCheapStocks(String[] stocks) {
+        int newArrayLength = 0;
+        for (int i = 0; i < stocks.length; i++) {
+            String[] newArr = stocks[i].split(" ");
+            int number1 = Integer.parseInt(newArr[1]);
+            if (number1 < 200) {
+//
+                newArrayLength++;
+            }
+        }
+        String[] resultArray = new String[newArrayLength];
+
+        int index = 0;
+        for (int i = 0; i < stocks.length; i++) {
+            String[] newArr = stocks[i].split(" ");
+            int number1 = Integer.parseInt(newArr[1]);
+            if (number1 < 200) {
+//                System.out.println("result.concat( newArr[0]) = " + result.concat(newArr[0]));
+                resultArray[index] = newArr[0];
+                index++;
+
+
+            }
+        }
+
+        String isReady1 = Arrays.toString(resultArray).replace("[", "");
+        String isReady2 = isReady1.replace(",", "");
+
+        return isReady2.replace("]", "");
+    }
+
+    public static void main(String[] args) {
+        CheapStocks stocks = new CheapStocks();
+
+        //Should be 144 - 20 + 50 + 40 + 34
+        int[] prices = new int[]{10, 20, 50, 40, 34, 500};
+        System.out.println(stocks.getCheapStocks(new String[]{"vys 743", "epd 11", "Ni 940", "ivn 545", "ba 165", "hiz 99", "juzo 793", "via 219", "um 857", "jo 331", "diz 76", "zini 166"}));
+    }
 }
