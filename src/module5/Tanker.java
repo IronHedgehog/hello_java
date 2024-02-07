ae324e4bac47e38ae7587de4ae0ef5d62e720b81
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-02-07 18:37:55 +0200

    space

--- src/module5/Tanker.java
+++ src/module5/Tanker.java
@@ -3,10 +3,18 @@ package module5;
 public class Tanker {
     private  String serialNumber;
 
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
     protected String getFuelType(){
         return "A500";
     }
     public void refuel(int amount){
-        System.out.println( "Add" + amount + "of" + getFuelType() +", tanker serial number is" +  serialNumber);
+        System.out.println( "Add " + amount + " of " + getFuelType() +", tanker serial number is " +  serialNumber);
     }
 }
