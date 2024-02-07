68d25a4d8d327adea0d84fb5b6b9585a6e164b58
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-02-07 23:35:51 +0200

    space

--- src/module5/Point.java
+++ src/module5/Point.java
@@ -1,4 +1,40 @@
 package module5;
 
-public class Point {
-}
+ class Point {
+  private int x;
+  private int y;
+  private int z;
+
+  public int getX() {
+   return x;
+  }
+
+  public void setX(int x) {
+   this.x = x;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+   if (this == obj) {
+    return true;
+   }else{
+    return false;
+   }
+  }
+
+  public int getY() {
+   return y;
+  }
+
+  public void setY(int y) {
+   this.y = y;
+  }
+
+  public int getZ() {
+   return z;
+  }
+
+  public void setZ(int z) {
+   this.z = z;
+  }
+ }
