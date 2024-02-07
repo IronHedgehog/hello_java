4d0ff32cfb186066249ae08894f1359a5b0d5172
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-02-07 18:13:31 +0200

    space

--- src/module5/Engine.java
+++ src/module5/Engine.java
@@ -1,4 +1,17 @@
 package module5;
 
 public class Engine {
+    private int power;
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getFuelType(){
+        return "A500";
+    }
 }
