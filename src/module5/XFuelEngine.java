106b84998a0e4f1c880e8295d976400c391197c2
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-02-07 18:18:58 +0200

    space

--- src/module5/XFuelEngine.java
+++ src/module5/XFuelEngine.java
@@ -1,4 +1,14 @@
 package module5;
 
-public class XFuelEngine {
+ class XFuelEngine extends Engine {
+     protected String serialNumber;
+
+     public void setSerialNumber(String serialNumber) {
+         this.serialNumber = serialNumber;
+     }
+
+     @Override
+    public String getFuelType() {
+        return "XFuel";
+    }
 }
