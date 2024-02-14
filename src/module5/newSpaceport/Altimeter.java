c82a3ea1cd5088320ce9d899cd47657188430cae
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-02-14 13:21:18 +0200

    module 5

--- src/module5/newSpaceport/Altimeter.java
+++ src/module5/newSpaceport/Altimeter.java
@@ -1,4 +1,31 @@
 package module5.newSpaceport;
 
 public class Altimeter {
+    @Override
+    public String toString() {
+        return "Measure height";
+    }
+}
+
+class AirGauge{
+    @Override
+    public String toString() {
+        return "Measure air pressure";
+    }
+}
+
+
+class ControlPanel{
+    private Altimeter altimeter = new Altimeter();
+    private AirGauge airGauge = new AirGauge();
+    public void control(){
+        System.out.println( altimeter.toString() + "\n" + airGauge.toString() );
+    }
+}
+
+
+class ControlPanelTest {
+    public static void main(String[] args) {
+        new ControlPanel().control();
+    }
 }
