be2c5819f2cb9b850e560d4584136d0a05cfb8c7
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-02-15 13:17:40 +0200

    module6

---
+++ src/module6/Bullet/Bullet.java
@@ -0,0 +1,10 @@
+package module6.Bullet;
+
+public class Bullet {
+    static {
+        System.out.println("GameSaver created!");
+    }
+    {
+        System.out.println("Bullet created!");
+    }
+}
