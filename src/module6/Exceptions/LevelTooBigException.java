cf08ac8db8f13e194c39aeed5d85e635636fec25
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-02-19 10:27:39 +0200

    module6

---
+++ src/module6/Exceptions/LevelTooBigException.java
@@ -0,0 +1,7 @@
+package module6.Exceptions;
+
+public class LevelTooBigException extends Exception {
+    public LevelTooBigException(String message) {
+        super(message);
+    }
+}
--- src/module6/TotalScore/TotalScore.java
+++ src/module6/TotalScore/TotalScore.java
@@ -1,4 +1,23 @@
 package module6.TotalScore;
 
-public class TotalScore {
+class TotalScore {
+    public int sum(int[] scores) {
+        int result = 0;
+        int index = 0;
+
+        while (index < scores.length) {
+            result += scores[index++];
+        }
+
+        return result;
+    }
+}
+
+class TotalScoreTest {
+    public static void main(String[] args) {
+        int[] score = {10, 20, 100};
+
+        //130
+        System.out.println(new TotalScore().sum(score));
+    }
 }
