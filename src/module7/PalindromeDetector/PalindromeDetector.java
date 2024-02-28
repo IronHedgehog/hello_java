daa5585b522ae16fcfffb7e328e851274080561a
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-02-28 11:21:02 +0200

    start

---
+++ src/module7/PalindromeDetector/PalindromeDetector.java
@@ -0,0 +1,26 @@
+package module7.PalindromeDetector;
+
+class PalindromeDetector {
+    public boolean isPalindrome(String word) {
+        return word.equalsIgnoreCase(reverse(word));
+    }
+
+    private String reverse(String word) {
+        StringBuilder result = new StringBuilder();
+        char[] chars = word.toCharArray();
+        for(char c: chars) {
+            result.insert(0, c);
+        }
+        return result.toString();
+    }
+}
+
+class PalindromeDetectorTest {
+    public static void main(String[] args) {
+        //false
+        System.out.println(new PalindromeDetector().isPalindrome("Java"));
+
+        //true
+        System.out.println(new PalindromeDetector().isPalindrome("Noon"));
+    }
+}
