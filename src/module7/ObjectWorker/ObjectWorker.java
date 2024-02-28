de4442c61e45854cfda8ac2f20c28a437c1a1301
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-02-28 11:24:19 +0200

    start

--- src/module7/ObjectWorker/ObjectWorker.java
+++ src/module7/ObjectWorker/ObjectWorker.java
@@ -1,4 +1,26 @@
 package module7.ObjectWorker;
 
-public class ObjectWorker {
+class ObjectWorker {
+    public String join(Object[] objects) {
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i < objects.length; i++) {
+            result.append(objects[i]);
+
+            if (i != objects.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+}
+
+class ObjectWorkerTest {
+    public static void main(String[] args) {
+        Object[] data = {"Hello", 20L, 3.14f, true};
+
+        //Hello 20 3.14 true
+        System.out.println(new ObjectWorker().join(data));
+    }
 }
